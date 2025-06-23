3ac0df0dda9039e7353aec24289ed5d38b087bfb
Anlor <chenxiex@outlook.com>
2025-06-23 10:00:07 +0800

    修复打包

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-from .main import get_random_id, get_post_data, send_post_data, get_comment_data
+from github_issue_forum.main import get_random_id, get_post_data, send_post_data, get_comment_data
+
+__all__ = [
+    "get_random_id",
+    "get_post_data",
+    "send_post_data",
+    "get_comment_data"
+]
